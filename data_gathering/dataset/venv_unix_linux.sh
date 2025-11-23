39db66fdd734b7fb37498b15b0990b538a57332c
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2025-11-22 20:51:52 -0500

    Changed project's directory structure and set up skeleton template. Added multiprocessing to feature extract script.

---
+++ data_gathering/dataset/venv_unix_linux.sh
@@ -0,0 +1,7 @@
+# Setting up the venv on Linux/macOS:
+# create a virtual environment
+python3 -m venv .venv
+# activate the virtual environment
+source .venv/bin/activate
+# install the required python libraries
+pip install -r requirements.txt
